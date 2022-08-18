9439b7b7eb6cdd9611ae3cae2cc5204aa96997eb
d-t-w <derek@troywest.com>
2022-08-18 19:10:25 +1000

    common src, test, dev-resources

--- slipway-jetty10/project.clj
+++ slipway-jetty10/project.clj
@@ -11,7 +11,7 @@
                                        [clj-http "3.12.3" :exclusions [commons-io]]
                                        [org.slf4j/slf4j-api "1.7.36"]
                                        [ch.qos.logback/logback-classic "1.2.11"]]
-                      :resource-paths ["dev-resources"]
+                      :resource-paths ["dev-resources" "common/dev-resources"]
                       :plugins        [[lein-cljfmt "0.8.2"]]}
              :kaocha {:dependencies [[lambdaisland/kaocha "1.69.1069"]]}
              :smoke  {:pedantic? :abort}}
@@ -32,4 +32,5 @@
                  [org.eclipse.jetty/jetty-jaas "10.0.11" :exclusions [org.slf4j/slf4j-api]]
                  [org.apache.mina/mina-core "2.1.6"]]       ;; explicit due to cve in 2.1.3 brought in by jetty-jaas 10.0.11
 
-  :source-paths ["src" "src-common"])
+  :source-paths ["src" "common/src"]
+  :test-paths ["test" "common/test"])
--- slipway-jetty9/project.clj
+++ slipway-jetty9/project.clj
@@ -11,7 +11,7 @@
                                        [clj-http "3.12.3" :exclusions [commons-io]]
                                        [org.slf4j/slf4j-api "1.7.36"]
                                        [ch.qos.logback/logback-classic "1.2.11"]]
-                      :resource-paths ["dev-resources"]
+                      :resource-paths ["dev-resources" "common/dev-resources"]
                       :plugins        [[lein-cljfmt "0.8.2"]]}
              :kaocha {:dependencies [[lambdaisland/kaocha "1.69.1069"]]}
              :smoke  {:pedantic? :abort}}
@@ -30,4 +30,5 @@
                  [org.eclipse.jetty.websocket/websocket-servlet "9.4.48.v20220622"]
                  [org.eclipse.jetty/jetty-jaas "9.4.48.v20220622"]]
 
-  :source-paths ["src" "src-common"])
+  :source-paths ["src" "common/src"]
+  :test-paths ["test" "common/test"])
