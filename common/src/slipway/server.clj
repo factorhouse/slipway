40c546a7c3b5db20aa57a490c2c73059b5eec051
d-t-w <derek@troywest.com>
2022-09-08 01:16:51 +1000

    simplify server ns

--- common/src/slipway/server.clj
+++ common/src/slipway/server.clj
@@ -1,36 +1,22 @@
 (ns slipway.server
   (:import (org.eclipse.jetty.server Connector Server)
-           (org.eclipse.jetty.util.thread QueuedThreadPool ScheduledExecutorScheduler ThreadPool)))
+           (org.eclipse.jetty.util.thread ScheduledExecutorScheduler ThreadPool)))
 
 (defmulti handler (fn [_ring_handler _login_service opts] (::handler opts)))
 
 (defmulti connector (fn [_server opts] (keyword (namespace (first (keys opts))) "connector")))
 
 (comment
-  #:slipway.server{:handler                 ""
-                   :max-threads             ""
-                   :min-threads             ""
-                   :threadpool-idle-timeout ""
-                   :job-queue               ""
-                   :daemon?                 ""
-                   :thread-pool             ""
-                   :error-handler           ""})
+  #:slipway.server{:handler       ""
+                   :connectors    ""
+                   :thread-pool   ""
+                   :error-handler ""})
 
 (defn create-server ^Server
-  [{::keys [max-threads min-threads threadpool-idle-timeout job-queue daemon? thread-pool error-handler connectors]
-    :or    {max-threads             50
-            min-threads             8
-            threadpool-idle-timeout 60000
-            job-queue               nil
-            daemon?                 false}}]
-  (let [pool   (or thread-pool
-                   (doto (QueuedThreadPool. (int max-threads)
-                                            (int min-threads)
-                                            (int threadpool-idle-timeout)
-                                            job-queue)
-                     (.setDaemon daemon?)))
-        server (doto (Server. ^ThreadPool pool)
-                 (.addBean (ScheduledExecutorScheduler.)))]
+  [{::keys [connectors thread-pool error-handler]}]
+  {:pre [connectors]}
+  (let [server (Server. ^ThreadPool thread-pool)]
     (.setConnectors server (into-array Connector (map #(connector server %) connectors)))
+    (.addBean server (ScheduledExecutorScheduler.))
     (when error-handler (.setErrorHandler server error-handler))
     server))
