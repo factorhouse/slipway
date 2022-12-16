9c2eb3144a307fed12a826bea4ca7adc038c2391
d-t-w <derek@troywest.com>
2022-12-16 13:06:35 +1100

    bump to latest jetty

--- slipway-jetty9/project.clj
+++ slipway-jetty9/project.clj
@@ -7,7 +7,7 @@
   :license {:name "MIT License"
             :url  "https://github.com/factorhosue/slipway/blob/main/LICENSE"}
 
-  :profiles {:dev   {:dependencies   [[clj-kondo "2022.11.02"]
+  :profiles {:dev   {:dependencies   [[clj-kondo "2022.12.08"]
                                       [clj-http "3.12.3" :exclusions [commons-io]]
                                       [ch.qos.logback/logback-classic "1.2.11"]
                                       [ring/ring-anti-forgery "1.3.0" :exclusions [crypto-random]]
@@ -24,10 +24,10 @@
                  [org.clojure/tools.logging "1.2.4"]
                  [ring/ring-servlet "1.9.5"]
                  [com.taoensso/sente "1.17.0"]
-                 [org.eclipse.jetty/jetty-server "9.4.49.v20220914"]
-                 [org.eclipse.jetty.websocket/websocket-server "9.4.49.v20220914"]
-                 [org.eclipse.jetty.websocket/websocket-servlet "9.4.49.v20220914"]
-                 [org.eclipse.jetty/jetty-jaas "9.4.49.v20220914"]
+                 [org.eclipse.jetty/jetty-server "9.4.50.v20221201"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.4.50.v20221201"]
+                 [org.eclipse.jetty.websocket/websocket-servlet "9.4.50.v20221201"]
+                 [org.eclipse.jetty/jetty-jaas "9.4.50.v20221201"]
                  [org.slf4j/slf4j-api "1.7.36"]]
 
   :source-paths ["src" "common/src" "common-javax/src"]
