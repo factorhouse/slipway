6e36ee6235e98a7762af9de241f6579067536faa
d-t-w <derek@troywest.com>
2023-03-02 16:36:27 +1100

    update to latest jetty

--- slipway-jetty10/project.clj
+++ slipway-jetty10/project.clj
@@ -7,7 +7,7 @@
   :license {:name "MIT License"
             :url  "https://github.com/factorhouse/slipway/blob/main/LICENSE"}
 
-  :profiles {:dev   {:dependencies   [[clj-kondo "2022.12.08" :exclusions [org.ow2.asm/asm]]
+  :profiles {:dev   {:dependencies   [[clj-kondo "2023.02.17" :exclusions [org.ow2.asm/asm]]
                                       [clj-http "3.12.3" :exclusions [commons-io]]
                                       [ch.qos.logback/logback-classic "1.2.11"]
                                       [ring/ring-anti-forgery "1.3.0" :exclusions [crypto-random]]
@@ -24,11 +24,11 @@
                  [org.clojure/tools.logging "1.2.4"]
                  [ring/ring-servlet "1.9.5"]
                  [com.taoensso/sente "1.17.0"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-api "10.0.13"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-server "10.0.13" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty.websocket/websocket-servlet "10.0.13" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-server "10.0.13" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-jaas "10.0.13" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty.websocket/websocket-jetty-api "10.0.14"]
+                 [org.eclipse.jetty.websocket/websocket-jetty-server "10.0.14" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty.websocket/websocket-servlet "10.0.14" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-server "10.0.14" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-jaas "10.0.14" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/slf4j-api "1.7.36"]]
 
   :source-paths ["common/src" "common-jetty1x/src" "common-javax/src"]
