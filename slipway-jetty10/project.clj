c48ad50b34a0552d9cc7c1adf22b73c23f35234b
d-t-w <derek@troywest.com>
2023-10-16 21:28:16 +1100

    update jetty10

--- slipway-jetty10/project.clj
+++ slipway-jetty10/project.clj
@@ -7,8 +7,8 @@
   :license {:name "MIT License"
             :url  "https://github.com/factorhouse/slipway/blob/main/LICENSE"}
 
-  :profiles {:dev   {:dependencies   [[com.fasterxml.jackson.core/jackson-core "2.15.2"] ;; required for internal inconsistency within clj-kondo
-                                      [clj-kondo "2023.07.13"]
+  :profiles {:dev   {:dependencies   [[com.fasterxml.jackson.core/jackson-core "2.15.3"] ;; required for internal inconsistency within clj-kondo
+                                      [clj-kondo "2023.09.07"]
                                       [clj-http "3.12.3" :exclusions [commons-io]]
                                       [ch.qos.logback/logback-classic "1.3.11"]
                                       [ring/ring-anti-forgery "1.3.0" :exclusions [crypto-random]]
@@ -25,11 +25,11 @@
                  [org.clojure/tools.logging "1.2.4"]
                  [ring/ring-servlet "1.9.5"]
                  [com.taoensso/sente "1.17.0"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-api "10.0.16"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-server "10.0.16" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty.websocket/websocket-servlet "10.0.16" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-server "10.0.16" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-jaas "10.0.16" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty.websocket/websocket-jetty-api "10.0.17"]
+                 [org.eclipse.jetty.websocket/websocket-jetty-server "10.0.17" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty.websocket/websocket-servlet "10.0.17" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-server "10.0.17" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-jaas "10.0.17" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/slf4j-api "2.0.9"]]
 
   :source-paths ["common/src" "common-jetty1x/src" "common-javax/src"]
--- slipway-jetty9/project.clj
+++ slipway-jetty9/project.clj
@@ -7,7 +7,7 @@
   :license {:name "MIT License"
             :url  "https://github.com/factorhosue/slipway/blob/main/LICENSE"}
 
-  :profiles {:dev   {:dependencies   [[com.fasterxml.jackson.core/jackson-core "2.15.2"] ;; required for internal inconsistency within clj-kondo
+  :profiles {:dev   {:dependencies   [[com.fasterxml.jackson.core/jackson-core "2.15.3"] ;; required for internal inconsistency within clj-kondo
                                       [clj-kondo "2023.09.07"]
                                       [clj-http "3.12.3" :exclusions [commons-io]]
                                       [ch.qos.logback/logback-classic "1.3.11"]
