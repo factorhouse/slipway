2190e234f6fc9a9364e392c1f7f7e75399f7bd68
d-t-w <derek@troywest.com>
2024-01-08 16:53:37 +1100

    update slipway-jetty11 deps

--- slipway-jetty11/project.clj
+++ slipway-jetty11/project.clj
@@ -7,12 +7,12 @@
   :license {:name "MIT License"
             :url  "https://github.com/factorhouse/slipway/blob/main/LICENSE"}
 
-  :profiles {:dev   {:dependencies   [[com.fasterxml.jackson.core/jackson-core "2.15.3"] ;; required for internal inconsistency within clj-kondo
-                                      [clj-kondo "2023.10.20"]
+  :profiles {:dev   {:dependencies   [[com.fasterxml.jackson.core/jackson-core "2.16.1"] ;; required for internal inconsistency within clj-kondo, kept at latest for CVE avoidance
+                                      [clj-kondo "2023.12.15"]
                                       [clj-http "3.12.3" :exclusions [commons-io commons-codec]]
-                                      [ch.qos.logback/logback-classic "1.3.11"]
-                                      [ring/ring-anti-forgery "1.3.0" :exclusions [crypto-random]]
-                                      [metosin/reitit-ring "0.5.18"]]
+                                      [ch.qos.logback/logback-classic "1.3.14"]
+                                      [ring/ring-anti-forgery "1.3.0" :exclusions [crypto-random crypto-equality]]
+                                      [metosin/reitit-ring "0.6.0"]]
                      :resource-paths ["dev-resources" "common/dev-resources"]
                      :plugins        [[lein-cljfmt "0.8.2"]]}
              :smoke {:pedantic? :abort}}
@@ -23,14 +23,14 @@
 
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.logging "1.2.4"]
-                 [ring/ring-servlet "1.9.5"]
+                 [ring/ring-servlet "1.9.6"]
                  [com.taoensso/sente "1.17.0"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-api "11.0.18"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-server "11.0.18" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty.websocket/websocket-servlet "11.0.18" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-server "11.0.18" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-jaas "11.0.18" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/slf4j-api "2.0.9"]]
+                 [org.eclipse.jetty.websocket/websocket-jetty-api "11.0.19"]
+                 [org.eclipse.jetty.websocket/websocket-jetty-server "11.0.19" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty.websocket/websocket-servlet "11.0.19" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-server "11.0.19" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-jaas "11.0.19" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/slf4j-api "2.0.10"]]
 
   :source-paths ["common/src" "common-jetty1x/src" "common-jakarta/src"]
   :test-paths ["test" "common/test"])
