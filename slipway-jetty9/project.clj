938bdbe759963b0c7d7332c1eedf8b4bcbaf838c
d-t-w <derek@troywest.com>
2024-06-04 16:45:15 +1000

    rm redundant jetty9 exclusions

--- slipway-jetty9/project.clj
+++ slipway-jetty9/project.clj
@@ -9,9 +9,9 @@
 
   :profiles {:dev   {:dependencies   [[com.fasterxml.jackson.core/jackson-core "2.16.2"] ;; required for internal inconsistency within clj-kondo, kept at latest for CVE avoidance
                                       [clj-kondo "2023.12.15"] ;; https://github.com/clj-kondo/clj-kondo/issues/2277 leave at this version until we move to Clojure 1.12.x and can fix
-                                      [clj-http "3.13.0" :exclusions [commons-io commons-codec]]
-                                      [ch.qos.logback/logback-classic "1.3.14"] ;; 1.3 branch is for Java EE / Java 8 so we will keep aligned here.
-                                      [ring/ring-anti-forgery "1.3.1" :exclusions [crypto-random crypto-equality]]
+                                      [clj-http "3.13.0"]
+                                      [ch.qos.logback/logback-classic "1.3.14"] ;; Logback 1.3.x supports the Java EE edition whereas logback 1.4.x supports Jakarta EE, otherwise the two versions are feature identical. The 1.5.x continues the 1.4.x series but with logback-access relocated to its own repository.
+                                      [ring/ring-anti-forgery "1.3.1"]
                                       [metosin/reitit-ring "0.7.0" :exclusions [ring/ring-core]]]
                      :resource-paths ["dev-resources" "common/dev-resources"]
                      :plugins        [[lein-cljfmt "0.9.2"]]}
