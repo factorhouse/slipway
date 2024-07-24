72516eb96dbb9a64c585f383697e6501adb8eecf
d-t-w <derek@troywest.com>
2024-07-24 14:17:09 +1000

    update Jetty11 dependencies to latest

--- slipway-jetty11/project.clj
+++ slipway-jetty11/project.clj
@@ -12,7 +12,7 @@
                                       [clj-http "3.13.0"]
                                       [ch.qos.logback/logback-classic "1.3.14"] ;; Logback 1.3.x supports the Java EE edition whereas logback 1.4.x supports Jakarta EE, otherwise the two versions are feature identical. The 1.5.x continues the 1.4.x series but with logback-access relocated to its own repository.
                                       [ring/ring-anti-forgery "1.3.1"]
-                                      [metosin/reitit-ring "0.7.0" :exclusions [ring/ring-core]]]
+                                      [metosin/reitit-ring "0.7.1" :exclusions [ring/ring-core]]]
                      :resource-paths ["dev-resources" "common/dev-resources"]
                      :plugins        [[lein-cljfmt "0.9.2"]]}
              :smoke {:pedantic? :abort}}
@@ -25,11 +25,11 @@
                  [org.clojure/tools.logging "1.3.0"]
                  [ring/ring-servlet "1.9.6"]
                  [com.taoensso/sente "1.17.0"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-api "11.0.21"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-server "11.0.21" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty.websocket/websocket-servlet "11.0.21" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-server "11.0.21" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-jaas "11.0.21" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty.websocket/websocket-jetty-api "11.0.22"]
+                 [org.eclipse.jetty.websocket/websocket-jetty-server "11.0.22" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty.websocket/websocket-servlet "11.0.22" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-server "11.0.22" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-jaas "11.0.22" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/slf4j-api "2.0.13"]]
 
   :source-paths ["common/src" "common-jetty1x/src" "common-jakarta/src"]
