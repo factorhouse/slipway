3affec42211a813101cb68648943924ea93d1d2e
d-t-w <derek@factorhouse.io>
2024-09-02 19:18:17 +1000

    update jetty11 deps

--- slipway-jetty11/project.clj
+++ slipway-jetty11/project.clj
@@ -1,4 +1,4 @@
-(defproject io.factorhouse/slipway-jetty11 "1.1.15"
+(defproject io.factorhouse/slipway-jetty11 "1.1.16"
 
   :description "A Clojure Companion for Jetty"
 
@@ -7,7 +7,7 @@
   :license {:name "MIT License"
             :url  "https://github.com/factorhouse/slipway/blob/main/LICENSE"}
 
-  :profiles {:dev   {:dependencies   [[com.fasterxml.jackson.core/jackson-core "2.16.2"] ;; required for internal inconsistency within clj-kondo, kept at latest for CVE avoidance
+  :profiles {:dev   {:dependencies   [[com.fasterxml.jackson.core/jackson-core "2.17.2"] ;; required for internal inconsistency within clj-kondo, kept at latest for CVE avoidance
                                       [clj-kondo "2023.12.15"]
                                       [clj-http "3.13.0"]
                                       [ch.qos.logback/logback-classic "1.3.14"] ;; Logback 1.3.x supports the Java EE edition whereas logback 1.4.x supports Jakarta EE, otherwise the two versions are feature identical. The 1.5.x continues the 1.4.x series but with logback-access relocated to its own repository.
@@ -21,16 +21,16 @@
             "kondo" ["with-profile" "+smoke" "run" "-m" "clj-kondo.main" "--lint" "common/src:common-jetty1x/src:test:common/test" "--parallel"]
             "fmt"   ["with-profile" "+smoke" "cljfmt" "check"]}
 
-  :dependencies [[org.clojure/clojure "1.11.3"]
+  :dependencies [[org.clojure/clojure "1.11.4"]
                  [org.clojure/tools.logging "1.3.0"]
                  [ring/ring-servlet "1.9.6"]
                  [com.taoensso/sente "1.17.0"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-api "11.0.22"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-server "11.0.22" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty.websocket/websocket-servlet "11.0.22" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-server "11.0.22" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-jaas "11.0.22" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/slf4j-api "2.0.13"]]
+                 [org.eclipse.jetty.websocket/websocket-jetty-api "11.0.23"]
+                 [org.eclipse.jetty.websocket/websocket-jetty-server "11.0.23" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty.websocket/websocket-servlet "11.0.23" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-server "11.0.23" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-jaas "11.0.23" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/slf4j-api "2.0.16"]]
 
   :source-paths ["common/src" "common-jetty1x/src" "common-jakarta/src"]
   :test-paths ["test" "common/test"])
