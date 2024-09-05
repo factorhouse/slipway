af9348b7b8ebca1b9718d26799f22064200a1a3b
d-t-w <derek@factorhouse.io>
2024-09-05 21:21:51 +1000

    update jetty10 deps

--- slipway-jetty10/project.clj
+++ slipway-jetty10/project.clj
@@ -1,4 +1,4 @@
-(defproject io.factorhouse/slipway-jetty10 "1.1.16"
+(defproject io.factorhouse/slipway-jetty10 "1.1.17"
 
   :description "A Clojure Companion for Jetty"
 
@@ -12,7 +12,7 @@
                                       [clj-http "3.13.0"]
                                       [ch.qos.logback/logback-classic "1.3.14"] ;; Logback 1.3.x supports the Java EE edition whereas logback 1.4.x supports Jakarta EE, otherwise the two versions are feature identical. The 1.5.x continues the 1.4.x series but with logback-access relocated to its own repository.
                                       [ring/ring-anti-forgery "1.3.1"]
-                                      [metosin/reitit-ring "0.7.1" :exclusions [ring/ring-core]]]
+                                      [metosin/reitit-ring "0.7.2" :exclusions [ring/ring-core]]]
                      :resource-paths ["dev-resources" "common/dev-resources"]
                      :plugins        [[lein-cljfmt "0.9.2"]]}
              :smoke {:pedantic? :abort}}
@@ -25,11 +25,11 @@
                  [org.clojure/tools.logging "1.3.0"]
                  [ring/ring-servlet "1.9.6"]
                  [com.taoensso/sente "1.17.0"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-api "10.0.23"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-server "10.0.23" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty.websocket/websocket-servlet "10.0.23" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-server "10.0.23" :exclusions [org.slf4j/slf4j-api]]
-                 [org.eclipse.jetty/jetty-jaas "10.0.23" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty.websocket/websocket-jetty-api "10.0.24"]
+                 [org.eclipse.jetty.websocket/websocket-jetty-server "10.0.24" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty.websocket/websocket-servlet "10.0.24" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-server "10.0.24" :exclusions [org.slf4j/slf4j-api]]
+                 [org.eclipse.jetty/jetty-jaas "10.0.24" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/slf4j-api "2.0.16"]]
 
   :source-paths ["common/src" "common-jetty1x/src" "common-javax/src"]
