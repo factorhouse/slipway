339379984f401fd4daf7cc8323645016fe95f3fd
Derek Troy-West <derek@factorhouse.io>
2025-08-26 21:10:32 +1000

    update jetty11

--- slipway-jetty10/project.clj
+++ slipway-jetty10/project.clj
@@ -8,7 +8,7 @@
             :url  "https://github.com/factorhouse/slipway/blob/main/LICENSE"}
 
   :profiles {:dev   {:dependencies   [[clj-kondo "2025.07.28" :exclusions [org.clojure/tools.reader]]
-                                      [clj-http "3.13.0" :exclusions [commons-codec commons-io]] ;; later version brought in by jetty-jaas and ring-servlet respectively
+                                      [clj-http "3.13.1" :exclusions [commons-codec commons-io]] ;; later version brought in by jetty-jaas and ring-servlet respectively
                                       [ch.qos.logback/logback-classic "1.3.15"] ;; Logback 1.3.x supports the Java EE edition whereas logback 1.4.x supports Jakarta EE, otherwise the two versions are feature identical. The 1.5.x continues the 1.4.x series but with logback-access relocated to its own repository.
                                       [ring/ring-anti-forgery "1.4.0"]
                                       [metosin/reitit-ring "0.7.2" :exclusions [ring/ring-core]]]
