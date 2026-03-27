0b5ec73fdff8d83324616171b2c70f08a0c70584
Derek Troy-West <derek@factorhouse.io>
2026-03-27 18:07:39 +1100

    fix deps

--- slipway-jetty12/project.clj
+++ slipway-jetty12/project.clj
@@ -25,9 +25,10 @@
 
   :dependencies [[org.clojure/clojure "1.12.4"]
                  [org.clojure/tools.logging "1.3.1"]
-                 [org.ring-clojure/ring-core-protocols "1.15.3"]
+                 [ring/ring-core "1.15.1"]
                  [ring/ring-defaults "0.7.0"]
                  [ring/ring-anti-forgery "1.4.0"]
+                 [org.ring-clojure/ring-core-protocols "1.15.3"]
                  [com.taoensso/sente "1.21.0"]
                  [org.eclipse.jetty.websocket/jetty-websocket-jetty-api "12.1.7"]
                  [org.eclipse.jetty.websocket/jetty-websocket-jetty-server "12.1.7"]
