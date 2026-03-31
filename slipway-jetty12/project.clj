2d68764b192a4ac6e4a7e16fc1703b9a72ae89de
Derek Troy-West <derek@factorhouse.io>
2026-03-31 22:21:57 +1100

    move ring deps to :dev profile, bump lein fmt

--- slipway-jetty12/project.clj
+++ slipway-jetty12/project.clj
@@ -11,9 +11,11 @@
                                       [clj-http "3.13.1" :exclusions [commons-io]] ;; later version in reitit-ring
                                       [ch.qos.logback/logback-classic "1.5.32"]
                                       [hiccup "2.0.0"]
+                                      [ring/ring-core "1.15.3"]
+                                      [ring/ring-anti-forgery "1.4.0"]
                                       [metosin/reitit-ring "0.10.1"]]
                      :resource-paths ["dev-resources"]
-                     :plugins        [[dev.weavejester/lein-cljfmt "0.15.6"]]}
+                     :plugins        [[dev.weavejester/lein-cljfmt "0.16.3"]]}
              :smoke {:pedantic? :abort}}
 
   :aliases {"check"  ["with-profile" "+smoke" "check"]
@@ -25,8 +27,6 @@
 
   :dependencies [[org.clojure/clojure "1.12.4"]
                  [org.clojure/tools.logging "1.3.1"]
-                 [ring/ring-core "1.15.3"]
-                 [ring/ring-anti-forgery "1.4.0"]
                  [org.ring-clojure/ring-core-protocols "1.15.3"]
                  [com.taoensso/sente "1.21.0"]
                  [org.eclipse.jetty.websocket/jetty-websocket-jetty-api "12.1.7"]
