2f6685f7ff0b8d4f0bfc62610be60e388ef98836
Derek Troy-West <derek@factorhouse.io>
2026-07-31 22:04:53 +1000

    test for retrieving id from access token

--- test/unit/slipway/security/openid/authorization_code_flow_test.clj
+++ test/unit/slipway/security/openid/authorization_code_flow_test.clj
@@ -53,4 +53,23 @@
                                          "other" {"roles" ["x-role"]}}
                                         {::openid.jwt/user-roles-path   ["other" "roles"]
                                          ::openid.jwt/user-id-path      ["other" "id"]
-                                         ::openid.jwt/user-roles-source :id-token}))))
+                                         ::openid.jwt/user-roles-source :id-token})))
+
+  ;; different configured path for name and roles (id taken from access-token)
+  (is (= {:name                                  "y-name"
+          :roles                                 #{"x-role"}
+          :slipway.security.openid/id-token      {"other" {"id" "x-name"}
+                                                  "sub"   "factor-dev"}
+          :slipway.security.openid/access-token  {"other" {"id"    "y-name"
+                                                           "roles" ["x-role"]}
+                                                  "roles" ["1" "2" "3"]}
+          :slipway.security.openid/refresh-token nil
+          :slipway.security.openid/response      nil}
+         (authorization-code-flow/state (OpenIdCredentials. {"sub"   "factor-dev"
+                                                             "other" {"id" "x-name"}})
+                                        {"roles" ["1" "2" "3"]
+                                         "other" {"roles" ["x-role"]
+                                                  "id"    "y-name"}}
+                                        {::openid.jwt/user-roles-path ["other" "roles"]
+                                         ::openid.jwt/user-id-path    ["other" "id"]
+                                         ::openid.jwt/user-id-source  :access-token}))))
