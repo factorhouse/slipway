ebeff78f231e7243a3816a18e138e10a02926cbc
Derek Troy-West <derek@factorhouse.io>
2026-07-31 23:56:46 +1000

    openid client credentials integration test

---
+++ test/integration/slipway/security/openid_client_credentials_test.clj
@@ -0,0 +1,261 @@
+(ns slipway.security.openid-client-credentials-test
+  (:require [clj-http.client :as client]
+            [clojure.test :refer [deftest is testing]]
+            [slipway.connector.http :as http]
+            [slipway.context :as context]
+            [slipway.example.app :as app]
+            [slipway.security :as security]
+            [slipway.security.openid :as openid]
+            [slipway.security.openid.jwk :as openid.jwk]
+            [slipway.security.openid.jwk.rsa :as openid.jwk.rsa]
+            [slipway.security.openid.jwt.verification :as openid.jwt.verification]
+            [slipway.sente]
+            [slipway.server :as server]
+            [slipway.test-server :as test-server])
+  (:import (com.nimbusds.jose JOSEObjectType JWSAlgorithm JWSHeader$Builder)
+           (com.nimbusds.jose.crypto RSASSASigner)
+           (com.nimbusds.jose.jwk RSAKey)
+           (com.nimbusds.jwt JWTClaimsSet$Builder SignedJWT)
+           (java.time Instant)
+           (java.util Date List)))
+
+(defn signed-jwt ^SignedJWT
+  [^RSAKey rsa-key {:keys [typ jti iss aud sub iat exp]}]
+  (let [header  (-> (JWSHeader$Builder. JWSAlgorithm/RS256)
+                    (.type (JOSEObjectType. typ))
+                    (.keyID (.getKeyID rsa-key))
+                    (.build))
+        builder (JWTClaimsSet$Builder.)]
+    (when iss (.issuer builder iss))
+    (when jti (.jwtID builder jti))
+    (when aud
+      (if (instance? String aud)
+        (.audience builder ^String aud)
+        (.audience builder ^List aud)))
+    (when sub (.subject builder sub))
+    (when iat (.issueTime builder (Date/from (Instant/ofEpochSecond iat))))
+    (when exp (.expirationTime builder (Date/from (Instant/ofEpochSecond exp))))
+    (doto (SignedJWT. header (.build builder))
+      (.sign (RSASSASigner. (.toRSAPrivateKey rsa-key))))))
+
+(deftest client-credentials-flow
+
+  (try
+
+    (let [rsa-key (openid.jwk.rsa/jwk {})
+          now     (.getEpochSecond (Instant/now))]
+
+      (test-server/start!
+       #::server{:connector     {::http/port 3000}
+                 :handler       {::context/ring-handler              (app/handler)
+                                 ::security/handler                  "openid"
+                                 ::openid/authorization-flow         :client-credentials
+                                 ::openid.jwk/source                 :rsa
+                                 ::openid.jwk.rsa/key                rsa-key
+                                 ::openid.jwt.verification/exact-iss "http://localhost:8080/realms/master"
+                                 ::openid.jwt.verification/exact-aud "https://slipway.io/api" ;; <-- set in keycloak-realms-with-client.json
+                                 ::openid/constraint-mappings        app/constraints}
+                 :error-handler app/server-error-handler})
+
+      (testing "no authorization header"
+
+        (is (= 401
+               (:status (client/request {:url              "http://localhost:3000/user"
+                                         :method           "GET"
+                                         :throw-exceptions false})))))
+
+      (testing "malformed authorization header"
+
+        (is (= 401
+               (:status (client/request {:url              "http://localhost:3000/user"
+                                         :method           "GET"
+                                         :headers          {:authorization (str "Bearer gibberish")}
+                                         :throw-exceptions false})))))
+
+      (testing "valid token"
+
+        (let [token (-> (signed-jwt rsa-key
+                                    {:typ "at+jwt"
+                                     :iss "http://localhost:8080/realms/master"
+                                     :jti "trrtcc:f28c3021-a469-59d4-7c94-52e94357086d"
+                                     :aud "https://slipway.io/api"
+                                     :sub "slipway-user-x"
+                                     :iat now
+                                     :exp (+ now 120)})
+                        (.serialize))]
+          (is (= 200 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false}))))))
+
+      (testing "jwt signed by the a different key"
+
+        (let [token (-> (signed-jwt (openid.jwk.rsa/jwk {})
+                                    {:typ "at+jwt"
+                                     :iss "http://localhost:8080/realms/master"
+                                     :jti "trrtcc:f28c3021-a469-59d4-7c94-52e94357086d"
+                                     :aud "https://slipway.io/api"
+                                     :sub "slipway-user-x"
+                                     :iat now
+                                     :exp (+ now 120)})
+                        (.serialize))]
+          (is (= 401 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false}))))))
+
+      (testing "missing iss"
+
+        (let [token (-> (signed-jwt rsa-key
+                                    {:typ "at+jwt"
+                                     :jti "trrtcc:f28c3021-a469-59d4-7c94-52e94357086d"
+                                     :aud "https://slipway.io/api"
+                                     :sub "slipway-user-x"
+                                     :iat now
+                                     :exp (+ now 120)})
+                        (.serialize))]
+          (is (= 401 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false}))))))
+
+      (testing "acceptable token"
+
+        (let [token (-> (signed-jwt rsa-key
+                                    {:typ "at+jwt"
+                                     :iss "http://localhost:8080/realms/wrong-iss"
+                                     :jti "trrtcc:f28c3021-a469-59d4-7c94-52e94357086d"
+                                     :aud "https://slipway.io/api"
+                                     :sub "slipway-user-x"
+                                     :iat now
+                                     :exp (+ now 120)})
+                        (.serialize))]
+          (is (= 401 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false}))))))
+
+      (testing "missing aud"
+
+        (let [token (-> (signed-jwt rsa-key
+                                    {:typ "at+jwt"
+                                     :iss "http://localhost:8080/realms/master"
+                                     :jti "trrtcc:f28c3021-a469-59d4-7c94-52e94357086d"
+                                     :sub "slipway-user-x"
+                                     :iat now
+                                     :exp (+ now 120)})
+                        (.serialize))]
+          (is (= 401 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false}))))))
+
+      (testing "missing aud"
+
+        (let [token (-> (signed-jwt rsa-key
+                                    {:typ "at+jwt"
+                                     :iss "http://localhost:8080/realms/master"
+                                     :jti "trrtcc:f28c3021-a469-59d4-7c94-52e94357086d"
+                                     :aud "https://other-service.io/api"
+                                     :sub "slipway-user-x"
+                                     :iat now
+                                     :exp (+ now 120)})
+                        (.serialize))]
+          (is (= 401 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false}))))))
+
+      (testing "acceptable token"
+
+        (let [token (-> (signed-jwt rsa-key
+                                    {:typ "at+jwt"
+                                     :iss "http://localhost:8080/realms/master"
+                                     :jti "trrtcc:f28c3021-a469-59d4-7c94-52e94357086d"
+                                     :aud ["account" "https://slipway.io/api"]
+                                     :sub "slipway-user-x"
+                                     :iat now
+                                     :exp (+ now 120)})
+                        (.serialize))]
+          (is (= 200 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false}))))))
+
+      (testing "acceptable token"
+
+        (let [token (-> (signed-jwt rsa-key
+                                    {:typ "at+jwt"
+                                     :iss "http://localhost:8080/realms/master"
+                                     :jti "trrtcc:f28c3021-a469-59d4-7c94-52e94357086d"
+                                     :aud "https://slipway.io/api"
+                                     :sub "slipway-user-x"
+                                     :iat (- now 120)
+                                     :exp (- now 100)})
+                        (.serialize))]
+          (is (= 401 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false}))))))
+
+      (testing "acceptable token"
+
+        (let [token (-> (signed-jwt rsa-key
+                                    {:typ "at+jwt"
+                                     :iss "http://localhost:8080/realms/master"
+                                     :aud "https://slipway.io/api"
+                                     :sub "slipway-user-x"
+                                     :iat now
+                                     :exp (+ now 120)})
+                        (.serialize))]
+          (is (= 401 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false}))))))
+
+      (testing "missing sub"
+
+        (let [token (-> (signed-jwt rsa-key
+                                    {:typ "at+jwt"
+                                     :iss "http://localhost:8080/realms/master"
+                                     :jti "trrtcc:f28c3021-a469-59d4-7c94-52e94357086d"
+                                     :aud "https://slipway.io/api"
+                                     :iat now
+                                     :exp (+ now 120)})
+                        (.serialize))]
+          (is (= 401 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false}))))))
+      (testing "missing iat"
+
+        (let [token (-> (signed-jwt rsa-key
+                                    {:typ "at+jwt"
+                                     :iss "http://localhost:8080/realms/master"
+                                     :jti "trrtcc:f28c3021-a469-59d4-7c94-52e94357086d"
+                                     :aud "https://slipway.io/api"
+                                     :sub "slipway-user-x"
+                                     :exp (+ now 120)})
+                        (.serialize))]
+          (is (= 401 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false}))))))
+
+      (testing "acceptable token"
+
+        (let [token (-> (signed-jwt rsa-key
+                                    {:typ "at+jwt"
+                                     :iss "http://localhost:8080/realms/master"
+                                     :jti "trrtcc:f28c3021-a469-59d4-7c94-52e94357086d"
+                                     :aud "https://slipway.io/api"
+                                     :sub "slipway-user-x"
+                                     :iat now})
+                        (.serialize))]
+          (is (= 401 (:status (client/request {:url              "http://localhost:3000/user"
+                                               :method           "GET"
+                                               :headers          {:authorization (str "Bearer " token)}
+                                               :throw-exceptions false})))))))
+
+    (finally
+      (test-server/stop!))))
--- test/integration/slipway/test_server.clj
+++ test/integration/slipway/test_server.clj
@@ -26,6 +26,7 @@
   (stop!)
   (reset! state (slipway/start config)))
 
+;; ./scripts/keycloak.sh
 (defn start-with-openid-auth-code!
   []
   (start! #::server{:connector     {::http/port 3000}
@@ -54,18 +55,9 @@
                                     ::openid/constraint-mappings   app/constraints}
                     :error-handler app/server-error-handler}))
 
-;; Curl a client-credentials access token from local keycloak
-;
-;curl -v --location --request POST 'http://localhost:8080/realms/master/protocol/openid-connect/token' \
-;--header 'Content-Type: application/x-www-form-urlencoded' \
-;--data-urlencode 'grant_type=client_credentials' \
-;--data-urlencode 'client_id=slipway' \
-;--data-urlencode 'client_secret=81a0d6ea-1468-4b20-b115-fa68a8df9cf8'
-
-;; Curl a request to slipway operating with :client-credentials flow
-; curl -v localhost:3000/user \
-; -H "Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJJd3BkaHMtTkxST3pZc1hXN0NEbzA5eHkzU2Fkc2pFQlE4c09nUlFBSTBZIn0.eyJleHAiOjE3ODUxMzc2NzQsImlhdCI6MTc4NTEzNzM3NCwianRpIjoidHJydGNjOjM2YmU4NmQzLWQ4N2YtYzBkZC1iMWJiLTYxNGQ5NjJkNjI4MiIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC9yZWFsbXMvbWFzdGVyIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU2OGRhNTc0LTUwZTgtNDk4Zi1iNzYzLTY1NWE0YjVlNTU1ZCIsInR5cCI6IkJlYXJlciIsImF6cCI6InNsaXB3YXkiLCJhY3IiOiIxIiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtbWFzdGVyIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiZW1haWwgcHJvZmlsZSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiY2xpZW50SG9zdCI6IjE3Mi4xNy4wLjEiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXJ2aWNlLWFjY291bnQtc2xpcHdheSIsImNsaWVudEFkZHJlc3MiOiIxNzIuMTcuMC4xIiwiY2xpZW50X2lkIjoic2xpcHdheSJ9.rggo7RTFSa5vCCT8IlULSK4QTXQYVYUjszHbgF3qE1r-QIAsHpVgOcIMqgABcEAUH1LyeWI4Bn25hs04Cx4_s12wkRDSQbLDSEKjuLEwQ0ovMSav67vi3WjNoKX1i8B1bHorgBjwBSuh7WMPgU4Tl_w58HQ_vLmnRLNIG_8UOfjBhFf7RD6bME5GMY0FR3efpbO2-dXZK93fMvFBtNfbCJshC-Xp86kbkA7qm_K7OtW9UECGvLB799ubGgBpL5Pp8ImkwiWcaWDFyOX7b2XkLkdNediTymvOGg2us51g6ptjuISbceDBmOFRfjab9XRm_OVrLvF4l_QR5HXL6n9HNg"
-
+;; ./scripts/keycloak.sh
+;; ./scripts/keycloak-get-token.sh
+;; ./scripts/keycloak-use-token.sh
 (defn start-with-openid-client-creds!
   []
   (start! #::server{:connector     {::http/port 3000}
