90859ee1a37de86d42518c4086bcea6064cad8b6
Derek Troy-West <derek@factorhouse.io>
2026-08-04 16:04:08 +1000

    fix tests

--- test/unit/slipway/security/openid/authorization_code_flow_test.clj
+++ test/unit/slipway/security/openid/authorization_code_flow_test.clj
@@ -13,130 +13,130 @@
 
 (deftest state
 
-  (is (= {::principal/type       ::openid/principal
-          ::principal/name       "factor-dev"
-          ::user/roles           #{"1" "2" "3"}
-          ::user/expires-at      1311281970
-          ::openid/id-token      {"other" {"id" "x-name"}
-                                  "sub"   "factor-dev"}
-          ::openid/access-token  {"exp"   1311281970
-                                  "other" {"roles" ["x-role"]}
-                                  "roles" ["1" "2" "3"]}
-          ::openid/refresh-token nil
-          ::openid/response      nil}
+  (is (= {::principal/type      ::openid/principal
+          ::principal/name      "factor-dev"
+          ::user/roles          #{"1" "2" "3"}
+          ::user/expires-at     1311281970
+          ::openid/id-token     {"other" {"id" "x-name"}
+                                 "sub"   "factor-dev"}
+          ::openid/access-token {"exp"   1311281970
+                                 "other" {"roles" ["x-role"]}
+                                 "roles" ["1" "2" "3"]}
+          ::openid/response     nil}
          (munge-expiry
-          (authorization-code-flow/state (OpenIdCredentials. {"sub"   "factor-dev"
-                                                              "other" {"id" "x-name"}})
-                                         {"roles" ["1" "2" "3"]
-                                          "other" {"roles" ["x-role"]}
-                                          "exp"   1311281970}
-                                         {}))))
+          (authorization-code-flow/state
+           (OpenIdCredentials. {"sub"   "factor-dev"
+                                "other" {"id" "x-name"}})
+           {"roles" ["1" "2" "3"]
+            "other" {"roles" ["x-role"]}
+            "exp"   1311281970}
+           {}))))
 
   ;; different configured path for name and roles (default tokens)
-  (is (= {::principal/type       ::openid/principal
-          ::principal/name       "x-name"
-          ::user/roles           #{"x-role"}
-          ::user/expires-at      1311281970
-          ::openid/id-token      {"other" {"id" "x-name"}
-                                  "sub"   "factor-dev"}
-          ::openid/access-token  {"other" {"roles" ["x-role"]}
-                                  "roles" ["1" "2" "3"]
-                                  "exp"   1311281970}
-          ::openid/refresh-token nil
-          ::openid/response      nil}
+  (is (= {::principal/type      ::openid/principal
+          ::principal/name      "x-name"
+          ::user/roles          #{"x-role"}
+          ::user/expires-at     1311281970
+          ::openid/id-token     {"other" {"id" "x-name"}
+                                 "sub"   "factor-dev"}
+          ::openid/access-token {"other" {"roles" ["x-role"]}
+                                 "roles" ["1" "2" "3"]
+                                 "exp"   1311281970}
+          ::openid/response     nil}
          (munge-expiry
-          (authorization-code-flow/state (OpenIdCredentials. {"sub"   "factor-dev"
-                                                              "other" {"id" "x-name"}})
-                                         {"roles" ["1" "2" "3"]
-                                          "other" {"roles" ["x-role"]}
-                                          "exp"   1311281970}
-                                         {::openid.jwt/user-roles-path ["other" "roles"]
-                                          ::openid.jwt/user-id-path    ["other" "id"]}))))
+          (authorization-code-flow/state
+           (OpenIdCredentials. {"sub"   "factor-dev"
+                                "other" {"id" "x-name"}})
+           {"roles" ["1" "2" "3"]
+            "other" {"roles" ["x-role"]}
+            "exp"   1311281970}
+           {::openid.jwt/user-roles-path ["other" "roles"]
+            ::openid.jwt/user-id-path    ["other" "id"]}))))
 
   ;; different configured path for name and roles (roles taken from id-token)
-  (is (= {::principal/type       ::openid/principal
-          ::principal/name       "x-name"
-          ::user/roles           #{"x-from-id-token"}
-          ::user/expires-at      1311281970
-          ::openid/id-token      {"other" {"id"    "x-name"
-                                           "roles" ["x-from-id-token"]}
-                                  "sub"   "factor-dev"}
-          ::openid/access-token  {"other" {"roles" ["x-role"]}
-                                  "roles" ["1" "2" "3"]
-                                  "exp"   1311281970}
-          ::openid/refresh-token nil
-          ::openid/response      nil}
+  (is (= {::principal/type      ::openid/principal
+          ::principal/name      "x-name"
+          ::user/roles          #{"x-from-id-token"}
+          ::user/expires-at     1311281970
+          ::openid/id-token     {"other" {"id"    "x-name"
+                                          "roles" ["x-from-id-token"]}
+                                 "sub"   "factor-dev"}
+          ::openid/access-token {"other" {"roles" ["x-role"]}
+                                 "roles" ["1" "2" "3"]
+                                 "exp"   1311281970}
+          ::openid/response     nil}
          (munge-expiry
-          (authorization-code-flow/state (OpenIdCredentials. {"sub"   "factor-dev"
-                                                              "other" {"id"    "x-name"
-                                                                       "roles" ["x-from-id-token"]}})
-                                         {"roles" ["1" "2" "3"]
-                                          "other" {"roles" ["x-role"]}
-                                          "exp"   1311281970}
-                                         {::openid.jwt/user-roles-path   ["other" "roles"]
-                                          ::openid.jwt/user-id-path      ["other" "id"]
-                                          ::openid.jwt/user-roles-source :id-token}))))
+          (authorization-code-flow/state
+           (OpenIdCredentials. {"sub"   "factor-dev"
+                                "other" {"id"    "x-name"
+                                         "roles" ["x-from-id-token"]}})
+           {"roles" ["1" "2" "3"]
+            "other" {"roles" ["x-role"]}
+            "exp"   1311281970}
+           {::openid.jwt/user-roles-path   ["other" "roles"]
+            ::openid.jwt/user-id-path      ["other" "id"]
+            ::openid.jwt/user-roles-source :id-token}))))
 
   ;; different configured path for name and roles (id taken from access-token)
-  (is (= {::principal/type       ::openid/principal
-          ::principal/name       "y-name"
-          ::user/roles           #{"x-role"}
-          ::user/expires-at      1311281970
-          ::openid/id-token      {"other" {"id" "x-name"}
-                                  "sub"   "factor-dev"}
-          ::openid/access-token  {"other" {"id"    "y-name"
-                                           "roles" ["x-role"]}
-                                  "roles" ["1" "2" "3"]
-                                  "exp"   1311281970}
-          ::openid/refresh-token nil
-          ::openid/response      nil}
+  (is (= {::principal/type      ::openid/principal
+          ::principal/name      "y-name"
+          ::user/roles          #{"x-role"}
+          ::user/expires-at     1311281970
+          ::openid/id-token     {"other" {"id" "x-name"}
+                                 "sub"   "factor-dev"}
+          ::openid/access-token {"other" {"id"    "y-name"
+                                          "roles" ["x-role"]}
+                                 "roles" ["1" "2" "3"]
+                                 "exp"   1311281970}
+          ::openid/response     nil}
          (munge-expiry
-          (authorization-code-flow/state (OpenIdCredentials. {"sub"   "factor-dev"
-                                                              "other" {"id" "x-name"}})
-                                         {"roles" ["1" "2" "3"]
-                                          "other" {"roles" ["x-role"]
-                                                   "id"    "y-name"}
-                                          "exp"   1311281970}
-                                         {::openid.jwt/user-roles-path ["other" "roles"]
-                                          ::openid.jwt/user-id-path    ["other" "id"]
-                                          ::openid.jwt/user-id-source  :access-token}))))
+          (authorization-code-flow/state
+           (OpenIdCredentials. {"sub"   "factor-dev"
+                                "other" {"id" "x-name"}})
+           {"roles" ["1" "2" "3"]
+            "other" {"roles" ["x-role"]
+                     "id"    "y-name"}
+            "exp"   1311281970}
+           {::openid.jwt/user-roles-path ["other" "roles"]
+            ::openid.jwt/user-id-path    ["other" "id"]
+            ::openid.jwt/user-id-source  :access-token}))))
 
   ;; roles is scalar in the jwt, is presented as a single-element-set in the output
   ;; we encounter this occasionally with IdP that reduce single-role claims to a string
-  (is (= {::principal/type       ::openid/principal
-          ::principal/name       "factor-dev"
-          ::user/roles           #{"1"}
-          ::user/expires-at      1311281970
-          ::openid/id-token      {"other" {"id" "x-name"}
-                                  "sub"   "factor-dev"}
-          ::openid/access-token  {"other" {"roles" ["x-role"]}
-                                  "roles" "1"
-                                  "exp"   1311281970}
-          ::openid/refresh-token nil
-          ::openid/response      nil}
+  (is (= {::principal/type      ::openid/principal
+          ::principal/name      "factor-dev"
+          ::user/roles          #{"1"}
+          ::user/expires-at     1311281970
+          ::openid/id-token     {"other" {"id" "x-name"}
+                                 "sub"   "factor-dev"}
+          ::openid/access-token {"other" {"roles" ["x-role"]}
+                                 "roles" "1"
+                                 "exp"   1311281970}
+          ::openid/response     nil}
          (munge-expiry
-          (authorization-code-flow/state (OpenIdCredentials. {"sub"   "factor-dev"
-                                                              "other" {"id" "x-name"}})
-                                         {"roles" "1"
-                                          "other" {"roles" ["x-role"]}
-                                          "exp"   1311281970}
-                                         {})))))
+          (authorization-code-flow/state
+           (OpenIdCredentials. {"sub"   "factor-dev"
+                                "other" {"id" "x-name"}})
+           {"roles" "1"
+            "other" {"roles" ["x-role"]}
+            "exp"   1311281970}
+           {})))))
 
 (deftest no-exp-field-in-access-token
 
   ;; technically this shoudn't really happen, but if it does it just results in nil exp in the user principal
-  (is (= {::principal/type       ::openid/principal
-          ::principal/name       "factor-dev"
-          ::user/roles           #{"1"}
-          ::user/expires-at      nil
-          ::openid/id-token      {"other" {"id" "x-name"}
-                                  "sub"   "factor-dev"}
-          ::openid/access-token  {"other" {"roles" ["x-role"]}
-                                  "roles" "1"}
-          ::openid/refresh-token nil
-          ::openid/response      nil}
-         (authorization-code-flow/state (OpenIdCredentials. {"sub"   "factor-dev"
-                                                             "other" {"id" "x-name"}})
-                                        {"roles" "1"
-                                         "other" {"roles" ["x-role"]}}
-                                        {}))))
+  (is (= {::principal/type      ::openid/principal
+          ::principal/name      "factor-dev"
+          ::user/roles          #{"1"}
+          ::user/expires-at     nil
+          ::openid/id-token     {"other" {"id" "x-name"}
+                                 "sub"   "factor-dev"}
+          ::openid/access-token {"other" {"roles" ["x-role"]}
+                                 "roles" "1"}
+          ::openid/response     nil}
+         (authorization-code-flow/state
+          (OpenIdCredentials. {"sub"   "factor-dev"
+                               "other" {"id" "x-name"}})
+          {"roles" "1"
+           "other" {"roles" ["x-role"]}}
+          {}))))
