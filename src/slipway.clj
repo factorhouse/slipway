5ef2ec99f17155f753e1aa308a82b54e97b78f84
Derek Troy-West <derek@factorhouse.io>
2026-08-19 14:36:16 +1000

    improved jwt docstrings

--- src/slipway.clj
+++ src/slipway.clj
@@ -123,9 +123,9 @@
                               :algorithms "a sequence of :algorithm if accepting multiple JWS algorithms"}
 
   #:slipway.security.oidc.jwt{:user-roles-source      "the token containing user roles, either :access-token or :id-token (default is :access-token)"
-                              :user-roles-path        "the path within the :roles token to find user roles, default is [\"roles\"]"
+                              :user-roles-path        "the path within the source token to find user roles, default is [\"roles\"]"
                               :user-id-source         "the token containing user id, either :access-token or :id-token (default is :id-token)"
-                              :user-id-path           "the path within the :id-token token to find user name, default is [\"sub\"]"
+                              :user-id-path           "the path within the source token to find user name, default is [\"sub\"]"
                               :user-expiration-source "the token used for session expiration, either :access-token or :id-token (default is :access-token)"}
 
   #:slipway.security.oidc.jwt.at.verification{::exact-typ       "a sequence of acceptable 'typ' fields, default is ['at+jwt' 'application/at+jwt']"
