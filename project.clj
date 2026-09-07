8ccccb9c9e79dff9c1ad4f2f335af0f5f81512ed
Derek Troy-West <derek@factorhouse.io>
2026-09-07 20:30:40 +1000

    update jetty, logback, and clojure dependencies

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 
   :profiles {:dev      {:dependencies   [[clj-kondo "2026.08.04" :exclusions [org.clojure/tools.reader]] ;; conflict with sente
                                          [clj-http "3.13.1" :exclusions [commons-io]] ;; later version in reitit-ring
-                                         [ch.qos.logback/logback-classic "1.5.38"]
+                                         [ch.qos.logback/logback-classic "1.6.3"]
                                          [hiccup "2.0.0"]
                                          [ring/ring-core "1.15.5"]
                                          [ring/ring-anti-forgery "1.4.0"]
@@ -27,19 +27,19 @@
         slipway.security.oidc.jwt.processor
         slipway.security.oidc.user.principal]
 
-  :dependencies [[org.clojure/clojure "1.12.5"]
+  :dependencies [[org.clojure/clojure "1.12.6"]
                  [org.clojure/tools.logging "1.3.1"]
                  [org.ring-clojure/ring-core-protocols "1.15.5"]
                  [com.taoensso/sente "1.17.0"]
                  [com.nimbusds/nimbus-jose-jwt "10.9.1"]
-                 [org.eclipse.jetty/jetty-server "12.1.12"]
-                 [org.eclipse.jetty/jetty-session "12.1.12"]
-                 [org.eclipse.jetty/jetty-security "12.1.12"]
-                 [org.eclipse.jetty/jetty-openid "12.1.12"]
-                 [org.eclipse.jetty.websocket/jetty-websocket-jetty-api "12.1.12"]
-                 [org.eclipse.jetty.websocket/jetty-websocket-jetty-server "12.1.12"]
-                 [org.eclipse.jetty.compression/jetty-compression-server "12.1.12"]
-                 [org.eclipse.jetty.compression/jetty-compression-gzip "12.1.12"]]
+                 [org.eclipse.jetty/jetty-server "12.1.13"]
+                 [org.eclipse.jetty/jetty-session "12.1.13"]
+                 [org.eclipse.jetty/jetty-security "12.1.13"]
+                 [org.eclipse.jetty/jetty-openid "12.1.13"]
+                 [org.eclipse.jetty.websocket/jetty-websocket-jetty-api "12.1.13"]
+                 [org.eclipse.jetty.websocket/jetty-websocket-jetty-server "12.1.13"]
+                 [org.eclipse.jetty.compression/jetty-compression-server "12.1.13"]
+                 [org.eclipse.jetty.compression/jetty-compression-gzip "12.1.13"]]
 
   :source-paths ["src"]
   :test-paths ["test/unit" "test/integration"]
